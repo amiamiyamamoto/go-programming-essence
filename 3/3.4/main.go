31f96db0f17ade0cbe07ce4f05b5ad62f07a68b2
amiamiyamamoto <amiyamamoto11@gmail.com>
2024-07-11 16:59:54 +0900

    iota型

--- 3/3.4/main.go
+++ 3/3.4/main.go
@@ -6,19 +6,32 @@ import (
 )
 
 func main() {
+	{
+		const n = 1
+		//constは使われる場所でそれぞれ型が決まるためコンパイルエラーにならない
+		x := 1 + n
+		y := 1.2 + n
 
-	const n = 1
-	//constは使われる場所でそれぞれ型が決まるためコンパイルエラーにならない
-	x := 1 + n
-	y := 1.2 + n
+		z := 9223372036854775807
+		a := 1. //これでフロートになる
+		//それぞれの型を出力
+		fmt.Println(reflect.TypeOf(x), reflect.TypeOf(y), reflect.TypeOf(z), a)
 
-	z := 9223372036854775807
-	a := 1. //これでフロートになる
-	//それぞれの型を出力
-	fmt.Println(reflect.TypeOf(x), reflect.TypeOf(y), reflect.TypeOf(z), a)
+		//intサイズを調べる
+		//intサイズは実行環境のCPUが32ビットか64ビットかで変わる
+		const intSize = 32 << (^uint(0) >> 63)
+		fmt.Println(intSize)
+	}
 
-	//intサイズを調べる
-	//intサイズは実行環境のCPUが32ビットか64ビットかで変わる
-	const intSize = 32 << (^uint(0) >> 63)
-	fmt.Println(intSize)
+	fmt.Println("-----------------------")
+
+	// iota(列挙)
+	const (
+		Apple  = iota
+		Orenge = iota + iota
+		Banana
+		Grape = iota
+	)
+	fmt.Println(Apple, Orenge, Banana, Grape)
+
 }
