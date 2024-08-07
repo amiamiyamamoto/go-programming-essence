3061d6c87fd75f4ca77e83fb8e5e821015ae06a7
amiamiyamamoto <amiyamamoto11@gmail.com>
2024-08-08 00:04:31 +0900

    goroutine

--- 3/3.5/main.go
+++ 3/3.5/main.go
@@ -1,26 +1,25 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"sync"
+)
 
 func main() {
-	hiho()
-	hihoClosure()
+	var wg sync.WaitGroup
+	wg.Add(2)
 
-}
-
-func hiho() {
 	message := "hi"
-	go sendMessage(message) // 引数はキャプチャされるため、hiが出力される
-	message = "ho"
-}
-
-func hihoClosure() {
-	message := "hi"
+	go sendMessage(message, &wg) // 引数はキャプチャされるため、hiが出力される
 	go func() {
-		sendMessage(message)
+		sendMessage(message, &wg)
 	}()
 	message = "ho"
+
+	wg.Wait()
 }
-func sendMessage(msg string) {
+
+func sendMessage(msg string, wg *sync.WaitGroup) {
 	fmt.Println(msg)
+	wg.Done()
 }
