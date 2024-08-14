77c91b2aee1189b423faea597acd8ad022aca899
amiamiyamamoto <amiyamamoto11@gmail.com>
2024-08-14 14:41:46 +0900

    sync.Mutex

--- 3/3.5.4/main.go
+++ 3/3.5.4/main.go
@@ -6,6 +6,34 @@ import (
 )
 
 func main() {
+	incrementRaceCondition()
+	n := 0
+	var mu sync.Mutex
+	var wg sync.WaitGroup
+	wg.Add(2)
+
+	go func() {
+		defer wg.Done()
+		for i := 0; i < 1000; i++ {
+			mu.Lock()
+			n++
+			mu.Unlock()
+		}
+	}()
+
+	go func() {
+		defer wg.Done()
+		for i := 0; i < 1000; i++ {
+			mu.Lock()
+			n++
+			mu.Unlock()
+		}
+	}()
+	wg.Wait()
+	fmt.Println(n)
+}
+
+func incrementRaceCondition() {
 	n := 0
 
 	var wg sync.WaitGroup
@@ -26,5 +54,5 @@ func main() {
 	}()
 
 	wg.Wait()
-	fmt.Println(n)
+	fmt.Println(n) //2000を期待しているが、データ競合（race condition）が発生しているため、期待通りの値が出力されない
 }
