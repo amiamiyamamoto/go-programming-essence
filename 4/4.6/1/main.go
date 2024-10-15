0acd18407aabce9a77a69767d0d2988a0cb15126
amiamiyamamoto <amiyamamoto11@gmail.com>
2024-10-15 10:57:55 +0900

    functionalOptionsPattern

---
+++ 4/4.6/1/main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fop/server"
+	"log"
+	"os"
+	"time"
+)
+
+func main() {
+	f, err := os.Create("server.log")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer f.Close()
+
+	logger := log.New(f, "", log.LstdFlags)
+	svr := server.New("localhost", 8888,
+		server.WithTimeout(time.Minute),
+		server.WithLogger(logger),
+	)
+	if err := svr.Start(); err != nil {
+		log.Fatal(err)
+	}
+}
