e249915cc06c4f5ca94fe3c8601751f1858f5c46
amiamiyamamoto <amiyamamoto11@gmail.com>
2024-10-16 12:04:45 +0900

    埋め込まれたstructのメソッド呼び出し

---
+++ 4/4.9/3/main.go
@@ -0,0 +1,40 @@
+package main
+
+import "fmt"
+
+type Attr struct {
+	Name string
+	Age  int
+}
+
+func (a Attr) String() string {
+	return fmt.Sprintf("%s(%d)", a.Name, a.Age)
+}
+
+type AttrEx struct {
+	Name string
+}
+
+func (a AttrEx) String() string {
+	return fmt.Sprintf("(a.k.a. %s)", a.Name)
+}
+
+type Teacher struct {
+	Attr
+	AttrEx
+	Subject string
+}
+
+func main() {
+	teacher := Teacher{
+		Attr: Attr{
+			Name: "John Schwartz",
+			Age:  43,
+		},
+		AttrEx: AttrEx{
+			Name: "JS",
+		},
+		Subject: "Math",
+	}
+	fmt.Println(teacher.Attr.String(), teacher.AttrEx.String())
+}
