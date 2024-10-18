d3405fca9e157fe50704e3f6c21f0cbbf55b0636
amiamiyamamoto <amiyamamoto11@gmail.com>
2024-10-18 23:57:39 +0900

    メソッドを区別する場合

--- 5/5.1/1/main.go
+++ 5/5.1/1/main.go
@@ -1,13 +1,19 @@
 package main
 
 import (
-	"fmt"
 	"net/http"
 )
 
 func main() {
+	// http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+	// 	fmt.Fprintf(w, "Hello, World!")
+	// })
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Hello, World!")
+		switch r.Method {
+		case http.MethodGet:
+			//GETの処理
+		default:
+		}
 	})
 	http.ListenAndServe(":8080", nil)
 }
