6ee11383192ec548b28ac1866d7b687c1e93cab1
amiamiyamamoto <amiyamamoto11@gmail.com>
2024-10-23 22:57:27 +0900

    複数のmimetypeを設定

--- 5/5.1/8/main.go
+++ 5/5.1/8/main.go
@@ -6,12 +6,21 @@ import (
 )
 
 func main() {
-	// http.Handle("/", http.FileServer(http.Dir("./static")))
-
+	mimemap := map[string]string{
+		".xls":  "application/vnd.ms-excel",
+		".xlsx": "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
+		".ppt":  "application/vnd.ms-powerpoint",
+		".pptx": "application/vnd.openxmlformats-officedocument.presentationml.presentation",
+		".doc":  "application/msword",
+		".docx": "application/vnd.openxmlformats-officedocument.wordprocessingml.document",
+	}
 	fileserver := http.StripPrefix("/public/", http.FileServer(http.Dir("./static")))
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		if path.Ext(r.URL.Path) == ".xls" {
-			w.Header().Set("Content-Type", "application/vnd.ms-excel")
+		// if path.Ext(r.URL.Path) == ".xls" {
+		// 	w.Header().Set("Content-Type", "application/vnd.ms-excel")
+		// }
+		if typ, ok := mimemap[path.Ext(r.URL.Path)]; ok {
+			w.Header().Set("Content-Type", typ)
 		}
 		fileserver.ServeHTTP(w, r)
 	})
