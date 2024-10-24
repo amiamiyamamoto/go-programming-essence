0a946236e0585c60b425ccce69c9ac6e3c6c7c54
amiamiyamamoto <amiyamamoto11@gmail.com>
2024-10-24 17:13:12 +0900

    処理を重く

--- 6/6.2/1/main.go
+++ 6/6.2/1/main.go
@@ -1,20 +1,23 @@
 package main
 
 import (
+	"fmt"
 	"sync"
-	"time"
 )
 
-func doSomething(wg *sync.WaitGroup) {
+func doSomething(wg *sync.WaitGroup, id int) {
 	defer wg.Done()
-	time.Sleep(300 * time.Second)
+	for i := 0; i < 10000; i++ {
+		fmt.Printf("%d\n", id)
+	}
+	// time.Sleep(300 * time.Second)
 }
 
 func main() {
 	var wg sync.WaitGroup
 	for i := 0; i < 100; i++ {
 		wg.Add(1)
-		go doSomething(&wg)
+		go doSomething(&wg, i)
 	}
 	wg.Wait()
 }
