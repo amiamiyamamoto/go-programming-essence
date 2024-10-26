9b3670a43ef5987ace9b9a64bd4a48eb11e5e735
amiamiyamamoto <amiyamamoto11@gmail.com>
2024-10-26 10:32:56 +0900

    iの再定義をカット

--- 6/6.4/6.13/main.go
+++ 6/6.4/6.13/main.go
@@ -18,7 +18,7 @@ func main() {
 
 	for i := 1; i <= 100; i++ {
 		wg.Add(1)
-		i := i
+		// i := i//今は不要
 		go func() {
 			defer wg.Done()
 			u := fmt.Sprintf("http://example.com/api/users?id=%d", i)
