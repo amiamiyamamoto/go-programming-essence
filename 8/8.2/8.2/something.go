9261081bf479e5b536fe9448a41d1b704a293fb2
amiamiyamamoto <amiyamamoto11@gmail.com>
2024-10-28 22:12:08 +0900

    修正後のコードとのベンチマークの比較

--- 8/8.2/8.2/something.go
+++ 8/8.2/8.2/something.go
@@ -3,10 +3,10 @@ package something
 import "fmt"
 
 func makeSomething(n int) []string {
-	var r []string
+	var r = make([]string, n, n)
 	for i := 0; i < n; i++ {
-		//都度appendするのはアロケーションの効率が悪い
-		r = append(r, fmt.Sprintf("%05d 何か", i))
+		r[i] = fmt.Sprintf("%5d 何か", i)
+		// r = append(r, fmt.Sprintf("%05d 何か", i))
 	}
 	return r
 }
