893a0e63d92fcf7f6d1d3ed8aedf6a537d6023db
amiamiyamamoto <amiyamamoto11@gmail.com>
2024-10-29 22:27:39 +0900

    net/http/pprofの使用例

---
+++ 8/8.4/8.8/main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"sync"
+)
+
+func hevyFunc(wg *sync.WaitGroup) {
+	defer wg.Done()
+	s := make([]string, 3)
+	for i := 0; i < 1000000; i++ {
+		s = append(s, "magicval pandas")
+	}
+}
+
+func main() {
+	go func() {
+		log.Println(http.ListenAndServe("localhost:6060", nil))
+	}()
+
+	for {
+		var wg sync.WaitGroup
+		wg.Add(1)
+		go hevyFunc(&wg)
+		wg.Wait()
+	}
+}
