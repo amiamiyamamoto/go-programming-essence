42849c9a25e773a7985dcb3a234f6cffa242c471
amiamiyamamoto <amiyamamoto11@gmail.com>
2024-10-30 23:31:25 +0900

    zipfileURLを取得

--- 9/9.2/cmd/aozora-search/main.go
+++ 9/9.2/cmd/aozora-search/main.go
@@ -1,8 +1,15 @@
-package mai
+package main
 
 import (
 	"fmt"
 	"log"
+	"net/http"
+	"net/url"
+	"path"
+	"regexp"
+	"strings"
+
+	"github.com/PuerkitoBio/goquery"
 )
 
 type Entry struct {
@@ -14,14 +21,65 @@ type Entry struct {
 	ZipURL   string
 }
 
-func findrntries(siteURL string) ([]Entiry, error) {
-	//処理
+func findEntries(siteURL string) ([]Entry, error) {
+	res, err := http.Get(siteURL)
+	if err != nil {
+		return nil, err
+	}
+	defer res.Body.Close()
+	if res.StatusCode != 200 {
+		log.Fatalf("Failed to fetch page: %d %s", res.StatusCode, res.Status)
+	}
+	doc, err := goquery.NewDocumentFromReader(res.Body)
+	if err != nil {
+		return nil, err
+	}
+	pat := regexp.MustCompile(`.*/cards/([0-9]+)/card([0-9]+).html$`)
+	doc.Find("ol li a").Each(func(n int, elem *goquery.Selection) {
+		token := pat.FindStringSubmatch(elem.AttrOr("href", ""))
+		if len(token) != 3 {
+			return
+		}
+		pageURL := fmt.Sprintf("https://www.aozora.gr.jp/cards/%s/card%s.html", token[1], token[2])
+		_, zipURL := findAuthorAndZIP(pageURL)
+		println(zipURL)
+	})
+	return nil, nil //ひとまずnilを返す
+}
+func findAuthorAndZIP(siteURL string) (string, string) {
+	res, err := http.Get(siteURL)
+	if err != nil {
+		return "", ""
+	}
+	defer res.Body.Close()
+	if res.StatusCode != 200 {
+		log.Fatalf("Failed to fetch page: %d %s", res.StatusCode, res.Status)
+	}
+	doc, err := goquery.NewDocumentFromReader(res.Body)
+	if err != nil {
+		return "", ""
+	}
+	author := doc.Find("table[summary=作家データ] tr:nth-child(1) td:nth-child(2)").Text()
+
+	zipURL := ""
+	doc.Find("table.download a").Each(func(n int, elem *goquery.Selection) {
+		href := elem.AttrOr("href", "")
+		if strings.HasSuffix(href, ".zip") {
+			zipURL = href
+		}
+	})
+	u, err := url.Parse(siteURL)
+	if err != nil {
+		return author, ""
+	}
+	u.Path = path.Join(path.Dir(u.Path), zipURL)
+	return author, u.String()
 }
 
 func main() {
-	listURL := "https//www.aozora.gr.jp/index_pages/person879.html"
+	listURL := "https://www.aozora.gr.jp/index_pages/person879.html"
 
-	entries, err := findEntries(lisURL)
+	entries, err := findEntries(listURL)
 	if err != nil {
 		log.Fatal(err)
 	}
