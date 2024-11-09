442cfdca1fb67be470d27a8befe25d5ae1e0ef8e
amiamiyamamoto <amiyamamoto11@gmail.com>
2024-11-09 20:48:14 +0900

    テーブルの見た目を変える

--- 9/9.7/9.33/main.go
+++ 9/9.7/9.33/main.go
@@ -9,13 +9,23 @@ import (
 func main() {
 	data := [][]string{
 		{"A", "The Good", "500"},
-		{"B", "The Very  very Bad Man", "288"},
+		{"B", "The Very very Bad Man", "288"},
 		{"C", "The Ugly", "120"},
 		{"D", "The Gopher", "800"},
 	}
 
 	table := tablewriter.NewWriter(os.Stdout)
 	table.SetHeader([]string{"Name", "sign", "Rating"})
+	table.SetColumnAlignment([]int{
+		tablewriter.ALIGN_CENTER,
+		tablewriter.ALIGN_DEFAULT,
+		tablewriter.ALIGN_DEFAULT,
+	})
+	table.SetHeaderColor(
+		tablewriter.Colors{tablewriter.Bold, tablewriter.BgGreenColor},
+		tablewriter.Colors{tablewriter.FgHiRedColor, tablewriter.Bold, tablewriter.BgBlackColor},
+		tablewriter.Colors{tablewriter.BgRedColor, tablewriter.FgWhiteColor},
+	)
 
 	for _, v := range data {
 		table.Append(v)
