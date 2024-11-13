7b198517221d759bfcdc168682be31f9c27ef873
amiamiyamamoto <amiyamamoto11@gmail.com>
2024-11-13 23:29:49 +0900

    bun

--- 10/10.2/10.1/main.go
+++ 10/10.2/10.1/main.go
@@ -5,14 +5,21 @@ import (
 	"time"
 
 	"github.com/labstack/echo/v4"
+	"github.com/uptrace/bun"
 )
 
+//bunのBaseModelを埋め込む
+
 type Todo struct {
-	ID        int64
-	Context   string
-	Done      bool
-	Until     time.Time
+	bun.BaseModel `bun:"table:todos,alias:t"`
+
+	ID        int64     `bun:"id.pk,autoincrement"`
+	Context   string    `bun:"content, notnull"`
+	Done      bool      `bun:"done"`
+	Until     time.Time `bun:"until,nullzero"`
 	CreatedAt time.Time
+	UpdatedAt time.Time `bun:",nullzero"`
+	DeletedAt time.Time `bun:",soft_delete,nullzero"`
 }
 
 func main() {
