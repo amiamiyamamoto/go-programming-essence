7fc57d3a635e15cece5231e7e6af24af26345734
amiamiyamamoto <amiyamamoto11@gmail.com>
2024-11-18 00:33:59 +0900

    POST処理を追加

--- 10/10.2/10.1/main.go
+++ 10/10.2/10.1/main.go
@@ -22,7 +22,7 @@ type Todo struct {
 	bun.BaseModel `bun:"table:todos,alias:t"`
 
 	ID        int64     `bun:"id,pk,autoincrement"`
-	Context   string    `bun:"content,notnull"`
+	Content   string    `bun:"content,notnull"`
 	Done      bool      `bun:"done"`
 	Until     time.Time `bun:"until,nullzero"`
 	CreatedAt time.Time
@@ -65,5 +65,68 @@ func main() {
 		}
 		return c.Render(http.StatusOK, "index", Data{Todos: todos})
 	})
+
+	e.POST("/", func(c echo.Context) error {
+		var todo Todo
+		// フォームパラメータをフィールドにバインド
+		errs := echo.FormFieldBinder(c).
+			Int64("id", &todo.ID).
+			String("content", &todo.Content).
+			Bool("done", &todo.Done).
+			CustomFunc("until", customFunc(&todo)).
+			BindErrors()
+		if errs != nil {
+			e.Logger.Error(errs)
+			return c.Render(http.StatusBadRequest, "index", Data{Errors: errs})
+		} else if todo.ID == 0 {
+			// IDが0のときは登録
+			ctx := context.Background()
+			if todo.Content == "" {
+				err = errors.New("Todo not found")
+			} else {
+				_, err = db.NewInsert().Model(&todo).Exec(ctx)
+				if err != nil {
+					e.Logger.Error(err)
+					err = errors.New("Cannot update")
+				}
+			}
+		} else {
+			ctx := context.Background()
+			if c.FormValue("delete") != "" {
+				// 削除
+				_, err = db.NewDelete().Model(&todo).Where("id = ?", todo.ID).Exec(ctx)
+			} else {
+				// 更新
+				var orig Todo
+				err = db.NewSelect().Model(&orig).Where("id = ?", todo.ID).Scan(ctx)
+				if err == nil {
+					orig.Done = todo.Done
+					_, err = db.NewUpdate().Model(&orig).Where("id = ?", todo.ID).Exec(ctx)
+				}
+			}
+			if err != nil {
+				e.Logger.Error(err)
+				err = errors.New("Cannot update")
+			}
+		}
+		if err != nil {
+			return c.Render(http.StatusBadRequest, "index", Data{Errors: []error{err}})
+		}
+		return c.Redirect(http.StatusFound, "/")
+	})
 	e.Logger.Fatal(e.Start(":8989"))
+}
+
+func customFunc(todo *Todo) func([]string) []error {
+	return func(values []string) []error {
+		if len(values) == 0 || values[0] == "" {
+			return nil
+		}
+		dt, err := time.Parse("2006-01-02T15:04 MST", values[0]+" JST")
+		if err != nil {
+			return []error{echo.NewBindingError("until", values[0:1], "failed to decode time", err)}
+		}
+		todo.Until = dt
+		return nil
+	}
 }
