9be28799da045458edf905967714de7ea7b55854
amiamiyamamoto <amiyamamoto11@gmail.com>
2024-11-26 16:12:46 +0900

    メール送信処理追加

--- 10/10.3/10.8/main.go
+++ 10/10.3/10.8/main.go
@@ -1,9 +1,13 @@
 package main
 
 import (
+	"bytes"
 	"context"
 	"database/sql"
+	"fmt"
 	"log"
+	"net/mail"
+	"net/smtp"
 	"os"
 	"time"
 
@@ -38,6 +42,38 @@ func main() {
 	var todos []Todo
 	ctx := context.Background()
 	err = db.NewSelect().Model(&todos).Order("created_at").Where("until is not nill").Where("done is false").Scan(ctx)
+	if err != nil {
+		log.Fatal(err)
+	}
+	if len(todos) == 0 {
+		return
+	}
+
+	// メール送信
+	from := mail.Address{Name: "TODO Reminder", Address: os.Getenv("MAIL_FROM")}
+	var buf bytes.Buffer
+	buf.WriteString("From: " + from.String() + "\r\n")
+	buf.WriteString("To: " + os.Getenv("MAIL_TO") + "\r\n")
+	buf.WriteString("Subject: TODO Reminder\r\n")
+	buf.WriteString("\r\n")
+	buf.WriteString("This is your todo list\n\n")
+	for _, todo := range todos {
+		fmt.Fprintf(&buf, "%s %s\n", todo.Until, todo.Content)
+	}
+
+	smtpAuth := smtp.PlainAuth(
+		os.Getenv("MAIL_DOMAIN"),
+		os.Getenv("MAIL_USER"),
+		os.Getenv("MAIL_PASSWORD"),
+		os.Getenv("MAIL_AUTHSERVER"),
+	)
+	err = smtp.SendMail(
+		os.Getenv("MAIL_SERVER"),
+		smtpAuth,
+		from.Address,
+		[]string{os.Getenv("MAIL_TO")},
+		buf.Bytes(),
+	)
 	if err != nil {
 		log.Fatal(err)
 	}
